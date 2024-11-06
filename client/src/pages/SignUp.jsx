a2dc86faea9c45cb5866d70550e17dfbf80113aa
Rabah-Muhammed <rabarabi31@gmail.com>
2024-11-06 19:36:41 +0530

    complete sign in page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom'
+import {Link,useNavigate} from 'react-router-dom'
 
 function SignUp() {
 
   const [formData,setFormData] = useState({});
   const [error,setError] = useState(false)
   const [loading,setLoading] = useState(false)
+  const navigate = useNavigate()
 
   const handleChange = (e)=>{
     setFormData({...formData,
@@ -32,6 +33,7 @@ function SignUp() {
         setError(true);
         return;
       }
+      navigate('/sign-in')
     } catch (error) {
       setLoading(false)
       setError(true)
